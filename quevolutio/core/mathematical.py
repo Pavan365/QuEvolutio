9ca6e9f4814c3e426d5bef6ed453dc8a9c2c78a4
Pavan <pavan.alpha.omega@gmail.com>
2025-08-12 18:05:16 +0100

    feat(core): add function for calculating the conversion matrix between Newtonian interpolation coefficients and Taylor-like derivatives

--- quevolutio/core/mathematical.py
+++ quevolutio/core/mathematical.py
@@ -303,6 +303,53 @@ def ne_coefficients(
     return coefficients
 
 
+def ne_ta_conversion(time_axis: sim.RVector) -> sim.RMatrix:
+    """
+    Calculates the square (lower triangular) conversion matrix for converting
+    Newtonian interpolation coefficients to Taylor-like derivatives, across a
+    time interval. This function expects the time-axis grid (time points) to
+    be a domain of length four.
+
+    Parameters
+    ----------
+    time_axis: sim.RVector
+        The time-axis grid (time points) from which the Newtonian interpolation
+        coefficients were generated. The time points should be from a domain of
+        length four (rescaled otherwise).
+
+    Returns
+    -------
+    conversion: sim.RMatrix
+        The conversion matrix.
+    """
+
+    # Store the number of expansion terms.
+    order: int = time_axis.shape[0]
+
+    # Account for the length four domain.
+    domain_factor: float = 4.0 / (time_axis[-1] - time_axis[0])
+
+    # Set up the conversion matrix.
+    conversion: sim.RMatrix = np.zeros((order, order), dtype=np.float64)
+    conversion[0, 0] = 1.0
+
+    # Construct the complete matrix.
+    for i in range(1, order):
+        # Calculate the m = 0 term (Semi-Global Appendix C.1).
+        conversion[i, 0] = -domain_factor * time_axis[i - 1] * conversion[i - 1, 0]
+
+        # Calculate the 1 <= m <= n - 1 terms (Semi-Global Appendix C.1).
+        for j in range(1, i):
+            conversion[i, j] = domain_factor * (
+                conversion[i - 1, j - 1] - (time_axis[i - 1] * conversion[i - 1, j])
+            )
+
+        # Calculate the m = n term (Semi-Global Appendix C.1).
+        conversion[i, i] = domain_factor * conversion[i - 1, i - 1]
+
+    return conversion
+
+
 def rescale_tensor(
     tensor: sim.RTensor, a: float, b: float
 ) -> tuple[sim.RTensor, float, float]:
