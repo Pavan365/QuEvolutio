75e494c5b6edebbbc751204eb490a169e094e229
Pavan <pavan.alpha.omega@gmail.com>
2025-08-12 21:55:13 +0100

    feat(propagators): add helper functions for the Semi-Global proapgator

--- quevolutio/propagators/semi_global.py
+++ quevolutio/propagators/semi_global.py
@@ -11,6 +11,10 @@ References
 from enum import Enum
 from typing import Optional, cast
 
+# Import external modules.
+import numpy as np
+from scipy.special import factorial
+
 # Import local modules.
 import quevolutio.core.simulation as sim
 
@@ -172,3 +176,123 @@ class SemiGlobalTDSE(sim.TDSE):
         difference: sim.GTensor = action(state, controls_1) - action(state, controls_2)
 
         return cast(sim.CTensor, self.prefactor * difference)
+
+
+def inhomogeneous_operator(
+    nodes: sim.GVector, time: float, order: int, threshold: float, tolerance: float
+) -> sim.CVector:
+    """
+    Evaluates the function of the inhomogeneous operator on a set of given
+    nodes, at a specified time point. This function represents the residual of
+    the time evolution operator, minus its truncated Taylor expansion.
+
+    + Semi-Global Section 2.3.2
+
+    Parameters
+    ----------
+    nodes : sim.GVector
+        The nodes to evaluate the function of the inhomogeneous operator on. In
+        general, these should be generated from either Chebyshev-Gauss or
+        Chebyshev-Lobatto nodes.
+    time : float
+        The time point at which to evaluate the function of the inhomogeneous
+        operator on.
+    order : int
+        The order of the truncated Taylor expansion of the inhomogeneous
+        operator.
+    threshold : float
+        The threshold above which to use the exact definition of the function
+        of the inhomogeneous operator. A stable definition is used if any of
+        the (nodes x time) points fall below the threshold.
+    tolerance : float
+        The tolerance for the stable function definition of the inhomogeneous
+        operator. This is used to truncate a Taylor series when convergence is
+        reached.
+
+    Returns
+    -------
+    sim.CVector
+        The evaluated inhomogeneous operator function values.
+    """
+
+    # Stable definition (Taylor).
+    if np.any(np.abs(nodes * time) < threshold):
+        term_sta_1: float = time**order
+
+        term_sta_2: sim.GVector = cast(
+            sim.GVector, np.zeros(nodes.shape[0], dtype=nodes.dtype)
+        )
+        max_expansion: int = 100
+
+        for i in range(max_expansion):
+            term: sim.GVector = ((nodes * time) ** i) / factorial(i + order)
+            term_sta_2 += term
+
+            if np.all(np.abs(term) < tolerance):
+                break
+
+        return factorial(order) * term_sta_1 * term_sta_2
+
+    # Standard definition.
+    else:
+        term_std_1: sim.GVector = nodes**-order
+        term_std_2: sim.GVector = np.exp(nodes * time)
+
+        term_std_3: sim.GVector = cast(
+            sim.GVector, np.zeros(nodes.shape[0], dtype=nodes.dtype)
+        )
+
+        for i in range(order):
+            term_std_3 += ((nodes * time) ** i) / factorial(i)
+
+        return factorial(order) * term_std_1 * (term_std_2 - term_std_3)
+
+
+def inhomogeneous_states(
+    state: sim.GTensor,
+    operator: sim.Operator,
+    derivatives: sim.GTensors,
+    controls: Optional[sim.Controls] = None,
+) -> sim.CVectors:
+    """
+    Calculates the inhomogeneous states which represent time-evolved states
+    with corrections from the time derivatives of an inhomogeneous term. The
+    derivatives should be calculated through converting Chebyshev expansion
+    coefficients or Newtonian interpolation coefficients to Taylor-like
+    derivative terms. The number of states to calculate is taken to be the
+    number of derivates plus one.
+
+    + Semi-Global Section 2.4.2
+
+    Parameters
+    ----------
+    state : sim.GTensor
+        The state being acted upon by the operator.
+    operator : sim.Operator
+        The operator being expanded. This should be a function that returns the
+        action of the operator on the state.
+    derivatives: sim.GTensors
+        The Taylor-like derivatives of an inhomogeneous term. The zeroth axis
+        is taken to be the axis of increasing derivative order.
+    controls : Optional[sim.Controls]
+        The controls that determine the structure of the operator. This should
+        be passed if the operator has explicit time dependence.
+
+    Returns
+    -------
+    states: sim.CTensors
+        The inhomogeneous states.
+    """
+
+    # Store the number of expansion terms.
+    order: int = derivatives.shape[0] + 1
+
+    # Set up the inhomogeneous states.
+    states: sim.CTensor = np.zeros((order, *state.shape), dtype=np.complex128)
+    states[0] = state.copy()
+
+    # Calculate the inhomogeneous states.
+    for i in range(1, order):
+        states[i] = (operator(states[i - 1], controls) + derivatives[i - 1]) / i
+
+    return states
