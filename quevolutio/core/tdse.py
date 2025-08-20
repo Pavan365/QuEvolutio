babbdd10c6fad34e7f2568bceadb7b5d39f7ea04
Pavan <pavan.alpha.omega@gmail.com>
2025-08-20 18:34:04 +0100

    feat(core): add TDSE class

--- quevolutio/core/tdse.py
+++ quevolutio/core/tdse.py
@@ -4,10 +4,10 @@ system.
 """
 
 # Import standard modules.
-from typing import Mapping, Optional, Protocol, Sequence, TypeAlias
+from typing import Mapping, Optional, Protocol, Sequence, TypeAlias, cast
 
 # Import local modules.
-from quevolutio.core.aliases import GTensor
+from quevolutio.core.aliases import CTensor, GTensor
 from quevolutio.core.domain import QuantumHilbertSpace
 
 # Type aliases for controls (time-dependent parameters).
@@ -154,3 +154,126 @@ class Source(Protocol):
         """
 
         ...
+
+
+class TDSE:
+    """
+    Represents a time-dependent Schrödinger equation (TDSE). This class aims to
+    encapsulate the right-hand side of the TDSE. This class is designed for use
+    in the time evolution of quantum states.
+
+    Parameters
+    ----------
+    domain : QuantumHilbertSpace
+        The discretised Hilbert space (domain) of the quantum system.
+    hamiltonian : Hamiltonian
+        The Hamiltonian of the quantum system.
+    source : Optional[Source]
+        The source term of the quantum system. This is an optional term that
+        can be excluded.
+
+    Attributes
+    ----------
+    Parameters
+    ----------
+    domain : QuantumHilbertSpace
+        The discretised Hilbert space (domain) of the quantum system.
+    hamiltonian : Hamiltonian
+        The Hamiltonian of the quantum system.
+    source : Optional[Source]
+        The source term of the quantum system. This is an optional term that
+        can be excluded.
+    """
+
+    def __init__(
+        self,
+        domain: QuantumHilbertSpace,
+        hamiltonian: Hamiltonian,
+        source: Optional[Source] = None,
+    ) -> None:
+        # Assign attributes.
+        self.domain: QuantumHilbertSpace = domain
+        self.hamiltonian: Hamiltonian = hamiltonian
+        self.source: Optional[Source] = source
+
+        # Calculate the homogeneous pre-factor.
+        self.prefactor: complex = -1j / self.domain.constants.hbar
+
+    def __call__(self, state: GTensor, controls: Optional[Controls] = None) -> CTensor:
+        """
+        Calculates the right-hand side of the time-dependent Schrödinger
+        equation (TDSE). This is the combination of the homogeneous term and an
+        optional source term.
+
+        Parameters
+        ----------
+        state : GTensor
+            The state being acted on. This should have shape
+            (*domain.num_points).
+        controls : Optional[Controls]
+            The controls which determine the structure of the TDSE. This should
+            be passed if the TDSE has explicit time dependence.
+
+        Returns
+        -------
+        CTensor
+            The right-hand side of the TDSE.
+        """
+
+        # Calculate the homogeneous term.
+        homogeneous: CTensor = self.homogeneous_term(state, controls)
+
+        # Calculate the source term if it is defined.
+        return (
+            homogeneous if self.source is None else homogeneous + self.source(controls)
+        )
+
+    def homogeneous_term(
+        self, state: GTensor, controls: Optional[Controls] = None
+    ) -> CTensor:
+        """
+        Calculates the homogeneous term of the time-dependent Schrödinger
+        equation (TDSE). If the Hamiltonian has explicit time dependence, a set
+        of controls should be passed.
+
+        Parameters
+        ----------
+        state : GTensor
+            The state being acted on. This should have shape
+            (*domain.num_points).
+        controls : Optional[Controls]
+            The controls which determine the structure of the TDSE. This should
+            be passed if the Hamiltonian has explicit time dependence.
+
+        Returns
+        -------
+        CTensor
+            The homogeneous term of the TDSE.
+        """
+
+        return cast(CTensor, self.prefactor * self.hamiltonian(state, controls))
+
+    def source_term(self, controls: Optional[Controls] = None) -> Optional[GTensor]:
+        """
+        Calculates the source term of the time-dependent Schrödinger equation
+        (TDSE). If the source term has explicit time dependence, a set of
+        controls should be passed.
+
+        Parameters
+        ----------
+        controls : Optional[Controls]
+            The controls which determine the structure of the TDSE. This should
+            be passed if the source term has explicit time dependence.
+
+        Returns
+        -------
+        Optional[GTensor]
+            The source term of the TDSE. None is returned if the source term is
+            not defined.
+        """
+
+        # If the source term is not defined, return None.
+        if self.source is None:
+            return None
+
+        return self.source(controls)
