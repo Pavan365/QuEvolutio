b2bcc906211f0ece030ab05d90eecc72ae3ecbd7
Pavan <pavan.alpha.omega@gmail.com>
2025-08-21 13:05:22 +0100

    test: update tests for the quevolutio.mathematical.affine module

--- tests/test_affine.py
+++ tests/test_affine.py
@@ -32,3 +32,29 @@ def test_rescale_tensor():
     # Check that the affine transformation factors are correct.
     assert np.allclose(vector_rs, (scale * vector) + shift)
     assert np.allclose(vector, (vector_rs - shift) / scale)
+
+
+def test_rescale_matrix():
+    """
+    Tests for the affine.rescale_matrix function.
+    """
+
+    # Construct a matrix.
+    matrix = np.diag(np.linspace(-200, 200, 20, dtype=np.float64))
+
+    # Define a target domain and rescale the eigenvalue domain of the matrix.
+    a, b = -1.0, 1.0
+    matrix_rs, scale, shift = affine.rescale_matrix(matrix, a, b)
+
+    # Get the rescaled eigenvalue domain.
+    eigenvalues_rs = np.linalg.eigvalsh(matrix_rs)
+
+    # Check that the boundary values are correct.
+    assert np.isclose(np.min(eigenvalues_rs), a)
+    assert np.isclose(np.max(eigenvalues_rs), b)
+
+    # Check that the affine transformation factors are correct.
+    identity = np.identity(matrix.shape[0], dtype=matrix.dtype)
+
+    assert np.allclose(matrix_rs, (scale * matrix) + (shift * identity))
+    assert np.allclose(matrix, (matrix_rs - (shift * identity)) / scale)
