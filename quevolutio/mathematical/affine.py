03ab5fe1fca70f4ef73c519ed392341973b4bc75
Pavan <pavan.alpha.omega@gmail.com>
2025-08-21 12:54:08 +0100

    feat(affine): add function for rescaling the eigenvalue domain of a Hermitian matrix

--- quevolutio/mathematical/affine.py
+++ quevolutio/mathematical/affine.py
@@ -3,11 +3,14 @@ Functions for transforming the domains of mathematical objects using affine
 transformations.
 """
 
+# Import standard modules.
+from typing import cast
+
 # Import external modules.
 import numpy as np
 
 # Import local modules.
-from quevolutio.core.aliases import RTensor
+from quevolutio.core.aliases import RVector, RTensor, GMatrix
 
 
 def rescale_tensor(tensor: RTensor, a: float, b: float) -> tuple[RTensor, float, float]:
@@ -47,3 +50,47 @@ def rescale_tensor(tensor: RTensor, a: float, b: float) -> tuple[RTensor, float,
     tensor_rs: RTensor = (scale * tensor) + shift
 
     return tensor_rs, scale, shift
+
+
+def rescale_matrix(matrix: GMatrix, a: float, b: float) -> tuple[GMatrix, float, float]:
+    """
+    Rescales the eigenvalue domain of a Hermitian matrix to the interval [a, b]
+    using an affine transformation. This function also returns the scaling and
+    shifting factors used to perform the affine transformation.
+
+    Parameters
+    ----------
+    matrix : GMatrix
+        The Hermitian matrix to rescale.
+    a : float
+        The lower bound of the target domain.
+    b : float
+        The upper bound of the target domain.
+
+    Returns
+    -------
+    matrix_rs : GMatrix
+        The rescaled Hermitian matrix.
+    scale : float
+        The scale factor used in the affine transformation.
+    shift : float
+        The shift factor used in the affine transformation.
+    """
+
+    # Get the eigenvalues.
+    eigenvalues: RVector = cast(RVector, np.linalg.eigvalsh(matrix))
+    eigenvalues_min: float = eigenvalues[0]
+    eigenvalues_max: float = eigenvalues[-1]
+
+    # Calculate the scale and shift factors.
+    scale: float = (b - a) / (eigenvalues_max - eigenvalues_min)
+    shift: float = ((a * eigenvalues_max) - (b * eigenvalues_min)) / (
+        eigenvalues_max - eigenvalues_min
+    )
+
+    # Rescale the matrix.
+    matrix_rs: GMatrix = (scale * matrix) + (
+        shift * np.identity(matrix.shape[0], dtype=matrix.dtype)
+    )
+
+    return matrix_rs, scale, shift
