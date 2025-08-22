44dc2047c41024f7344aa1406f5403fb90de46b4
Pavan <pavan.alpha.omega@gmail.com>
2025-08-22 17:04:14 +0100

    feat(propagators): add SemiGlobalPropagator class and set up basic structure

--- quevolutio/propagators/semi_global.py
+++ quevolutio/propagators/semi_global.py
@@ -11,10 +11,33 @@ References
 from enum import Enum
 from typing import Optional, cast
 
+# Import external modules.
+import numpy as np
+from scipy.special import factorial
+
 # Import local modules.
-from quevolutio.core.aliases import CTensor, GTensor
-from quevolutio.core.domain import QuantumHilbertSpace
-from quevolutio.core.tdse import TDSE, Controls, Hamiltonian, Operator, Source
+import quevolutio.mathematical.affine as affine
+import quevolutio.mathematical.approximation as approx
+from quevolutio.core.aliases import (  # isort: skip
+    RVector,
+    CVector,
+    GVector,
+    CVectors,
+    RMatrix,
+    CTensor,
+    GTensor,
+    CTensors,
+    GTensors,
+)
+from quevolutio.core.domain import QuantumHilbertSpace, TimeGrid
+from quevolutio.core.tdse import (
+    TDSE,
+    Controls,
+    Hamiltonian,
+    Operator,
+    Source,
+    TDSEControls,
+)
 
 
 class ApproximationBasis(Enum):
@@ -67,7 +90,7 @@ class SemiGlobalTDSE(TDSE):
         domain: QuantumHilbertSpace,
         hamiltonian: Hamiltonian,
         source: Optional[Source] = None,
-    ):
+    ) -> None:
         # Construct the TDSE class.
         super().__init__(domain, hamiltonian, source)
 
@@ -155,3 +178,311 @@ class SemiGlobalTDSE(TDSE):
         difference: GTensor = action(state, controls_1) - action(state, controls_2)
 
         return cast(CTensor, self.prefactor * difference)
+
+
+class SemiGlobalPropagator:
+    """
+    Represents the Semi-Global propagation scheme.
+
+    Parameters
+    ----------
+    system : TDSE
+        The time-dependent Schrödinger equation (TDSE) of the quantum system.
+    time_domain : TimeGrid
+        The time domain (grid) over which to propagate.
+    order_m : int
+        The order of the Taylor-like expansion of the time-evolution operator
+        in the Semi-Global propagation scheme.
+    order_k : int
+        The order of the Chebyshev expansion used to approximate the correction
+        operator in the Semi-Global propagation scheme.
+    tolerance : float
+        The tolerance of the propagator. This is used to define the convergence
+        criterion during iterative time ordering.
+    approximation : ApproximationBasis
+        The approximation basis to use for the time-dependent inhomogeneous
+        term in the Semi-Global propagation scheme. This can either be a
+        Chebyshev basis approximation or a Newtonian basis approximation.
+
+    Attributes
+    ----------
+    system : SemiGlobalTDSE
+        The time-dependent Schrödinger equation (TDSE) of the quantum system.
+    time_domain : TimeGrid
+        The time domain (grid) over which to propagate.
+    order_m : int
+        The order of the Taylor-like expansion of the time-evolution operator
+        in the Semi-Global propagation scheme.
+    order_k : int
+        The order of the Chebyshev expansion used to approximate the correction
+        operator in the Semi-Global propagation scheme.
+    tolerance : float
+        The tolerance of the propagator. This is used to define the convergence
+        criterion during iterative time ordering.
+    approximation : ApproximationBasis
+        The approximation basis to use for the time-dependent inhomogeneous
+        term in the Semi-Global propagation scheme.
+
+    Internal Attributes
+    -------------------
+    _time_nodes : RVector
+        The complete propagation time grid constructed from Chebyshev-Lobatto
+        nodes. These contain the interior points on which terms are expanded in
+        time.
+    _correction_coefficients_curr : CVectors
+        The Chebyshev expansion coefficients of the correction operator for the
+        current time interval.
+    _correction_coefficients_next : CVectors
+        The Chebyshev expansion coefficients of the correction operator for the
+        next time interval.
+    _conversion_matrix : RMatrix
+        The conversion matrix used to convert the expansion coefficients of the
+        inhomogeneous term in the TDSE to Taylor-like derivative terms.
+    """
+
+    def __init__(
+        self,
+        system: TDSE,
+        time_domain: TimeGrid,
+        order_m: int,
+        order_k: int,
+        tolerance: float,
+        approximation: ApproximationBasis,
+    ) -> None:
+        # Assign attributes.
+        self._system: SemiGlobalTDSE = SemiGlobalTDSE(
+            system.domain, system.hamiltonian, system.source
+        )
+        self._time_domain: TimeGrid = time_domain
+
+        self._order_m: int = order_m
+        self._order_k: int = order_k
+        self._tolerance: float = tolerance
+        self._approximation: ApproximationBasis = approximation
+
+        # Perform pre-computations.
+        self.pre_computations()
+
+    ## NOTE: PROPERTIES --------------------------------------------------------
+
+    @property
+    def system(self) -> SemiGlobalTDSE:
+        return self._system
+
+    @property
+    def time_domain(self) -> TimeGrid:
+        return self._time_domain
+
+    @property
+    def order_m(self) -> int:
+        return self._order_m
+
+    @property
+    def order_k(self) -> int:
+        return self._order_k
+
+    @property
+    def tolerance(self) -> float:
+        return self._tolerance
+
+    @property
+    def approximation(self) -> ApproximationBasis:
+        return self._approximation
+
+    ## NOTE: PROPERTIES END ----------------------------------------------------
+
+    def pre_computations(self) -> None:
+        """
+        Performs pre-computations of constant quantities in the Semi-Global
+        propagation scheme.
+        """
+
+        # Set up Chebyshev-Gauss and Chebyshev-Lobatto nodes.
+        cg_nodes: RVector = approx.ch_gauss_nodes(self._order_k)
+        cl_nodes: RVector = approx.ch_lobatto_nodes(self._order_m)
+
+        # Rescale the Chebyshev-Lobatto nodes to the first time interval.
+        cl_nodes_rs: RVector = affine.rescale_tensor(
+            cl_nodes, self._time_domain.time_min, self._time_domain.time_max
+        )[0]
+
+        # Set up the entire propagation time grid of Chebyshev-Lobatto nodes.
+        self._time_nodes: RVector = np.zeros(
+            (((self._order_m - 1) * (self._time_domain.num_points - 1)) + 1),
+            dtype=np.float64,
+        )
+        self._time_nodes[0] = self.time_domain.time_axis[0]
+        self._time_nodes[1:] = (
+            cl_nodes_rs[1::]
+            + (
+                np.arange(self._time_domain.num_points - 1) * self._time_domain.time_dt
+            ).reshape(-1, 1)
+        ).flatten(order="C")
+
+        # Set up the Chebyshev-Gauss nodes.
+        # For calculating the Chebyshev expansion coefficients of the correction operator.
+        correction_nodes: CVector = cast(
+            CTensor,
+            self._system.prefactor
+            * affine.rescale_tensor(
+                cg_nodes,
+                self._system.hamiltonian.eigenvalue_min,
+                self._system.hamiltonian.eigenvalue_max,
+            )[0],
+        )
+
+        # Evaluate the correction operator function on the first two time intervals.
+        # These can be reused because the effective time steps are constant.
+        correction_values_curr: CVectors = np.zeros(
+            (self._order_m, self._order_k), dtype=np.complex128
+        )
+        correction_values_next: CVectors = np.zeros(
+            (self._order_m, self._order_k), dtype=np.complex128
+        )
+
+        for i in range(self._order_m):
+            correction_values_curr[i] = self.correction_operator(
+                correction_nodes, self._time_nodes[i], threshold=1e-2
+            )
+            correction_values_next[i] = self.correction_operator(
+                correction_nodes,
+                self._time_nodes[i + (self._order_m - 1)],
+                threshold=1e-2,
+            )
+
+        # Calculate the Chebyshev expansion coefficients of the correction operator.
+        self._correction_coefficients_curr: CVectors = cast(
+            CVectors,
+            approx.ch_coefficients(correction_values_curr.T[::-1], dct_type=2).T,
+        )
+        self._correction_coefficients_next: CVectors = cast(
+            CVectors,
+            approx.ch_coefficients(correction_values_next.T[::-1], dct_type=2).T,
+        )
+
+        # Generate the conversion matrix.
+        # To convert expansion coefficients to Taylor-like derivatives.
+        # Chebyshev basis approximation.
+        if self._approximation == ApproximationBasis.CHEBYSHEV:
+            self._conversion_matrix: RMatrix = approx.ch_ta_conversion(
+                self._order_m,
+                self.time_domain.time_axis[0],
+                self.time_domain.time_axis[1],
+            ).T
+
+        # Newtonian basis approximation.
+        else:
+            self._conversion_matrix: RMatrix = approx.ne_ta_conversion(cl_nodes_rs).T
+
+    def correction_operator(
+        self, nodes: GVector, time: float, threshold: float
+    ) -> GVector:
+        """
+        Evaluates the correction operator function, which corrects the
+        Taylor-like expansion of the time-evolution operator in the Semi-Global
+        propagation scheme. This function calculates the residual between the
+        time-evolution operator and its Taylor expansion, scaled for the m-th
+        derivative.
+
+        + Semi-Global Paper Section 2.3.2
+
+        Parameters
+        ----------
+        nodes : GVector
+            The nodes on which to evaluate the correction operator function.
+        time : float
+            The time point at which to evaluate the correction operator
+            function. This can also represent a time step.
+        threshold : float
+            The threshold below which to use a stable Taylor expansion of the
+            correction operator function instead of the exact definition. The
+            stable definition is used if any |nodes * time| points fall below
+            threshold.
+
+        Returns
+        -------
+        GVector
+            The evaluated correction operator function.
+        """
+
+        # Pre-calculate reused quantities.
+        nodes_x_time: GVector = nodes * time
+
+        # Stable definition.
+        if np.any(np.abs(nodes_x_time) < threshold):
+            term_sta_1: float = time**self._order_m
+            term_sta_2: GVector = cast(
+                GVector, np.zeros(nodes.shape[0], dtype=nodes.dtype)
+            )
+
+            max_expansion: int = 100
+            for i in range(max_expansion):
+                term: GVector = (nodes_x_time**i) / factorial(i + self._order_m)
+                term_sta_2 += term
+
+                if np.all(np.abs(term) < np.finfo(np.float64).eps):
+                    break
+
+            correction_nodes: GVector = term_sta_1 * term_sta_2
+
+        # Standard definition.
+        else:
+            term_std_1: GVector = nodes**-self._order_m
+            term_std_2: GVector = np.exp(nodes_x_time)
+            term_std_3: GVector = cast(
+                GVector, np.zeros(nodes.shape[0], dtype=nodes.dtype)
+            )
+
+            for i in range(self._order_m):
+                term_std_3 += (nodes_x_time**i) / factorial(i)
+
+            correction_nodes: GVector = term_std_1 * (term_std_2 - term_std_3)
+
+        return factorial(self._order_m) * correction_nodes
+
+    def expansion_states(
+        self, state: GTensor, derivatives: GTensors, controls: Optional[Controls]
+    ) -> CTensors:
+        """
+        Constructs the expansion states used to construct a propagated state in
+        the Semi-Global propagation scheme. These states are used to construct
+        the Taylor-like expansion of the time-evolution operator propagating a
+        state, and the corresponding correction term. The states are made from
+        the homogeneous term of the time-dependent Schrödinger equation (TDSE)
+        and the time derivatives of the inhomogeneous term of the TDSE.
+
+        + Semi-Global Paper Section 2.4.2
+
+        Parameters
+        ----------
+        state : GTensor
+            The state being propagated by the time-evolution operator.
+        derivatives : GTensors
+            The Taylor-like time derivatives of the inhomogeneous term of the
+            TDSE. The zeroth axis is taken to be the axis of increasing order
+            derivatives.
+        controls : Optional[Controls]
+            The controls which determine the structure of the TDSE. This should
+            be passed if the Hamiltonian has explicit time dependence.
+
+        Returns
+        -------
+        CTensors
+            The expansion states used to construct a propagated state.
+        """
+
+        # Set the number of expansion terms.
+        order: int = self._order_m + 1
+
+        # Set up the expansion states.
+        states: CTensors = np.zeros((order, *state.shape), dtype=np.complex128)
+        states[0] = state.copy()
+
+        # Calculate the expansion states.
+        for i in range(1, order):
+            states[i] = (
+                self._system.homogeneous_term(states[i - 1], controls)
+                + derivatives[i - 1]
+            ) / i
+
+        return states
