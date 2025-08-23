4e4735d261fe31a5c2a926b05595598272620cf8
Pavan <pavan.alpha.omega@gmail.com>
2025-08-23 13:08:13 +0100

    feat(propagators): add Chebyshev class and implementation of the Chebyshev propagation scheme

--- quevolutio/propagators/chebyshev.py
+++ quevolutio/propagators/chebyshev.py
@@ -9,11 +9,16 @@ References
 """
 
 # Import standard modules.
-from typing import Any, Optional
+from typing import Any, Optional, cast
+
+# Import external modules.
+import numpy as np
 
 # Import local modules.
-from quevolutio.core.aliases import GTensor
-from quevolutio.core.tdse import Controls, Hamiltonian
+import quevolutio.mathematical.approximation as approx
+from quevolutio.core.aliases import CVector, CTensor, GTensor, CTensors  # isort: skip
+from quevolutio.core.domain import TimeGrid
+from quevolutio.core.tdse import Controls, Hamiltonian, TDSEControls
 
 
 class ChebyshevHamiltonian(Hamiltonian):
@@ -83,3 +88,150 @@ class ChebyshevHamiltonian(Hamiltonian):
         return (
             self(state, controls) - (self.spectrum_centre * state)
         ) / self.spectrum_half_span
+
+
+class Chebyshev:
+    """
+    Represents the Chebyshev propagation scheme. This scheme is intended to be
+    used for quantum systems that do not have explicit time-dependence.
+
+    Parameters
+    ----------
+    system : TDSE
+        The time-dependent Schrödinger equation (TDSE) of the quantum system.
+    time_domain : TimeGrid
+        The time domain (grid) over which to propagate.
+    order_k : int
+        The order of the Chebyshev expansion used to approximate the
+        time-evolution operator in the Chebyshev propagation scheme.
+
+    Attributes
+    ----------
+    system : ChebyshevTDSE
+        The time-dependent Schrödinger equation (TDSE) of the quantum system.
+    time_domain : TimeGrid
+        The time domain (grid) over which to propagate.
+    order_k : int
+        The order of the Chebyshev expansion used to approximate the
+        time-evolution operator in the Chebyshev propagation scheme.
+
+    Internal Attributes
+    -------------------
+    _prefactor : complex
+        The constant that multiplies the homogeneous term in the time-dependent
+        Schrödinger equation (TDSE).
+    _phase_factor : complex
+        The global phase factor applied at the end of each propagation step in
+        the Chebyshev propagation scheme.
+    _coefficients : CVector
+        The Chebyshev expansion coefficients of the time-evolution operator in
+        the Chebyshev propagation scheme.
+    """
+
+    def __init__(
+        self, hamiltonian: Hamiltonian, time_domain: TimeGrid, order_k: int
+    ) -> None:
+        # Assign attributes.
+        self._hamiltonian: ChebyshevHamiltonian = ChebyshevHamiltonian(hamiltonian)  # type: ignore
+        self._time_domain: TimeGrid = time_domain
+        self._order_k: int = order_k
+
+        # Calculate the homogeneous pre-factor (TDSE).
+        self._prefactor: complex = -1j / self._hamiltonian.domain.constants.hbar
+
+        # Pre-compute the global phase factor applied at each propagation step.
+        self._phase_factor: complex = np.exp(
+            self._prefactor
+            * self.time_domain.time_dt
+            * self._hamiltonian.spectrum_centre
+        )
+
+        # Pre-compute the Chebyshev expansion coefficients of the time-evolution operator.
+        bessel_argument: complex = (
+            self._time_domain.time_dt
+            * self._hamiltonian.spectrum_half_span
+            / self._hamiltonian.domain.constants.hbar
+        )
+        self._coefficients: CVector = approx.ch_bessel_coefficients(
+            bessel_argument, self._order_k
+        )
+
+    ## NOTE: PROPERTIES --------------------------------------------------------
+
+    @property
+    def hamiltonian(self) -> ChebyshevHamiltonian:
+        return self._hamiltonian
+
+    @property
+    def time_domain(self) -> TimeGrid:
+        return self._time_domain
+
+    @property
+    def order_k(self) -> int:
+        return self._order_k
+
+    ## NOTE: PROPERTIES END ----------------------------------------------------
+
+    def propagate(
+        self, state: GTensor, controls_fn: Optional[TDSEControls]
+    ) -> CTensors:
+        """
+        Propagates a state with respect to the time-dependent Schrödinger
+        equation (TDSE) using the Chebyshev propagation scheme.
+
+        Parameters
+        ----------
+        state : GTensor
+            The state to propagate with respect to the TDSE.
+        controls_fn : Optional[TDSEControls]
+            A callable that generates the controls which determine the
+            structure of the TDSE at a given time. This should be passed if the
+            TDSE has explicit time dependence.
+
+        Returns
+        -------
+        states : CTensors
+            The propagated states.
+        """
+
+        # Ensure that a controls callable is passed for a time-dependent system.
+        if self._hamiltonian.time_dependent and controls_fn is None:
+            raise ValueError("invalid controls callable")
+        assert controls_fn is not None
+
+        # Create an array to store the propagated states.
+        states: CTensors = np.zeros(
+            (self._time_domain.num_points, *self._hamiltonian.domain.num_points),
+            dtype=np.complex128,
+        )
+        states[0] = state.copy()
+
+        # Propagate the state.
+        for i in range(self._time_domain.num_points - 1):
+            # Store the current state.
+            state_curr: CTensor = states[i]
+
+            # If the Hamiltonian has explicit time dependence.
+            # Calculate the controls at the start of the time step.
+            controls: Optional[Controls] = None
+            if self._hamiltonian.time_dependent:
+                controls: Optional[Controls] = controls_fn(
+                    self._time_domain.time_axis[i]
+                )
+
+            # Calculate the Chebyshev expansion of the time-evolution operator.
+            # Acting on the current state.
+            state_next: CTensor = cast(
+                CTensor,
+                approx.ch_expansion(
+                    state_curr,
+                    self._hamiltonian.hamiltonian_rs,
+                    self._coefficients,
+                    controls,
+                ),
+            )
+
+            # Calculate and store the propagated state.
+            states[i + 1] = state_next * self._phase_factor
+
+        return states
